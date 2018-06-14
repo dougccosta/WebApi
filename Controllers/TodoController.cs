1d911c2cdac24a482d3ae12f5bc47447fcc505ac
douglas.costa <douglas.costa@benner.com.br>
2018-06-14 15:10:33 -0300

    Mini Web API finalizada

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -45,5 +45,65 @@ namespace TodoApi.Controllers
         }
         #endregion
         #endregion
+
+        #region Create
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new TodoItem{ Id = item.Id }, item);
+        }
+        #endregion
+
+        #region Update
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = _context.TodoItems.Find(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name       = item.Name;
+
+            _context.TodoItems.Update(todo);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+        #endregion
+
+        #region Delete
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.TodoItems.Find(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+        #endregion
     }
 }
